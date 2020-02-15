9c599dee37c077743942d513ea8929d69e36bd92
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-02-15 13:06:10 -0800

    use more robust script

--- nextcloud/entrypoint.sh
+++ nextcloud/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/run/supervisord
+# Create log dirs and files
+mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
+touch $( cat /etc/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
 
+# Then run supervisord
 /usr/bin/supervisord
