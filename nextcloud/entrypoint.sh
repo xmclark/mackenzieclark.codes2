d2cc2ae1f14adf94168a1f67b85da51d7e72f056
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-02-15 13:11:08 -0800

    and pid file

--- nextcloud/entrypoint.sh
+++ nextcloud/entrypoint.sh
@@ -2,7 +2,10 @@
 
 # Create log dirs and files
 mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
-touch $( cat /etc/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
+touch $( cat /etc/supervisor/conf.d/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
+
+mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// ) )
+touch $( cat /etc/supervisor/conf.d/supervisord.conf  | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// )
 
 # Then run supervisord
 /usr/bin/supervisord
