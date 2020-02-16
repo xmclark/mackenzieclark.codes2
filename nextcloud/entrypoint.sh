60c54ed7ea8376be1d9a9aaba18a80af0cd62195
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-02-15 17:36:09 -0800

    no more startup script

--- nextcloud/entrypoint.sh
+++ nextcloud/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Create log dirs and files
-mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
-touch $( cat /etc/supervisor/conf.d/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
-
-mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// ) )
-touch $( cat /etc/supervisor/conf.d/supervisord.conf  | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// )
+#mkdir -p $( dirname $(cat /supervisord.conf | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
+#touch $( cat /supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
+#
+#mkdir -p $( dirname $(cat /supervisord.conf | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// ) )
+#touch $( cat /supervisord.conf  | grep pidfile= | grep "\.pid" | sed s/.*pidfile=// )
 
 # Then run supervisord
 /usr/bin/supervisord
